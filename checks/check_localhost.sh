6e67239c41f9381a1bbfce496c1c767ac4d32d1e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-08 15:05:50 +0200

    No user needed to be specified

--- checks/check_localhost.sh
+++ checks/check_localhost.sh
@@ -2,18 +2,18 @@ ADDRESS=localhost:8080/api/v1
 
 mkdir -p localhost
 
-curl -k -v -u insights-qa:redhatqa $ADDRESS/ > localhost/main.json
-curl -k -v -u insights-qa:redhatqa $ADDRESS/groups > localhost/groups.json
-curl -k -v -u insights-qa:redhatqa $ADDRESS/organizations > localhost/organizations.json
+curl -k -v $ADDRESS/ > localhost/main.json
+curl -k -v $ADDRESS/groups > localhost/groups.json
+curl -k -v $ADDRESS/organizations > localhost/organizations.json
 
-curl -k -v -u insights-qa:redhatqa $ADDRESS/organizations/11789772/clusters > localhost/clusters_11789772.json
-curl -k -v -u insights-qa:redhatqa $ADDRESS/organizations/11940171/clusters > localhost/clusters_11940171.json
+curl -k -v $ADDRESS/organizations/11789772/clusters > localhost/clusters_11789772.json
+curl -k -v $ADDRESS/organizations/11940171/clusters > localhost/clusters_11940171.json
 
 clusters="34c3ecc5-624a-49a5-bab8-4fdc5e51a266 74ae54aa-6577-4e80-85e7-697cb646ff37 a7467445-8d6a-43cc-b82c-7007664bdf69 ee7d2bf4-8933-4a3a-8634-3328fe806e08"
 
 for cluster in $clusters
 do
-    curl -k -v -u insights-qa:redhatqa $ADDRESS/report/11789772/${cluster} > localhost/report_${cluster}.json
+    curl -k -v $ADDRESS/report/11789772/${cluster} > localhost/report_${cluster}.json
 done
 
 diff -r expected localhost
