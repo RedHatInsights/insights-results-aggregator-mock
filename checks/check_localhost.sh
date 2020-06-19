924833482ebd2cc78cc982fd369300103b2aeb10
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-19 13:41:45 +0200

    Updated check script

--- checks/check_localhost.sh
+++ checks/check_localhost.sh
@@ -9,7 +9,7 @@ curl -k -v $ADDRESS/organizations > localhost/organizations.json
 curl -k -v $ADDRESS/organizations/11789772/clusters > localhost/clusters_11789772.json
 curl -k -v $ADDRESS/organizations/11940171/clusters > localhost/clusters_11940171.json
 
-clusters="34c3ecc5-624a-49a5-bab8-4fdc5e51a266 74ae54aa-6577-4e80-85e7-697cb646ff37 a7467445-8d6a-43cc-b82c-7007664bdf69 ee7d2bf4-8933-4a3a-8634-3328fe806e08"
+clusters="34c3ecc5-624a-49a5-bab8-4fdc5e51a266 74ae54aa-6577-4e80-85e7-697cb646ff37 a7467445-8d6a-43cc-b82c-7007664bdf69 ee7d2bf4-8933-4a3a-8634-3328fe806e08 eeeeeeee-eeee-eeee-eeee-000000000001"
 
 for cluster in $clusters
 do
