ef7c6c76d85ca3a4c5ba6bbd1a5647ca05fb2c0c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-11 12:14:32 +0200

    Script to check if cluster list REST API endpoint is working

---
+++ checks/check_cluster_list.sh
@@ -0,0 +1,11 @@
+ADDRESS=localhost:8080/api/v1
+
+mkdir -p localhost
+
+curl -k -v $ADDRESS/clusters -d @cluster_list_1.json > localhost/report_list1.json
+curl -k -v $ADDRESS/clusters -d @cluster_list_2.json > localhost/report_list2.json
+curl -k -v $ADDRESS/clusters -d @cluster_list_3.json > localhost/report_list3.json
+curl -k -v $ADDRESS/clusters -d @cluster_list_4.json > localhost/report_list4.json
+curl -k -v $ADDRESS/clusters -d @cluster_list_5.json > localhost/report_list5.json
+curl -k -v $ADDRESS/clusters -d @cluster_list_6.json > localhost/report_list6.json
+curl -k -v $ADDRESS/clusters -d @cluster_list_7.json > localhost/report_list7.json
