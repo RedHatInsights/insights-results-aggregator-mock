2ea32bbf672785b317b4930aeecc91f01d88d22e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-22 14:53:11 +0200

    Fixed URL to gocyclo tool

--- gocyclo.sh
+++ gocyclo.sh
@@ -24,7 +24,7 @@ echo -e "${BLUE}Finding functions and methods with high cyclomatic complexity${N
 if ! [ -x "$(command -v gocyclo)" ]
 then
     echo -e "${BLUE}Installing gocyclo${NC}"
-    GO111MODULE=off go get github.com/fzipp/gocyclo
+    GO111MODULE=off go get github.com/fzipp/gocyclo/cmd/gocyclo
 fi
 
 if ! gocyclo -over 29 -avg .
