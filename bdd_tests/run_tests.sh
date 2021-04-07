3e9e774d34945a75ab0b4d9715923ce158d16621
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-07 10:20:25 +0200

    Fixed/commented issues found by shellcheck

--- bdd_tests/run_tests.sh
+++ bdd_tests/run_tests.sh
@@ -4,10 +4,12 @@ export PATH=$PATH:/tmp/ramdisk/
 export NOVENV=1
 
 function prepare_venv() {
-    virtualenv -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt
+    # shellcheck disable=SC1091
+    virtualenv -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
-PYTHONDONTWRITEBYTECODE=1 python3 "`which behave`" --tags=-skip -D dump_errors=true @feature_list.txt $@
+# shellcheck disable=SC2068
+PYTHONDONTWRITEBYTECODE=1 python3 "$(which behave)" --tags=-skip -D dump_errors=true @feature_list.txt $@
 
