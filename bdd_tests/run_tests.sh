b1a7d2e01278af450caab7d8c45f340ceaf9ec24
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-07 10:25:55 +0200

    Use command -v instead of which

--- bdd_tests/run_tests.sh
+++ bdd_tests/run_tests.sh
@@ -5,11 +5,11 @@ export NOVENV=1
 
 function prepare_venv() {
     # shellcheck disable=SC1091
-    virtualenv -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install -r requirements.txt
+    virtualenv -p python3 venv && source venv/bin/activate && python3 "$(command -v pip3)" install -r requirements.txt
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
 # shellcheck disable=SC2068
-PYTHONDONTWRITEBYTECODE=1 python3 "$(which behave)" --tags=-skip -D dump_errors=true @feature_list.txt $@
+PYTHONDONTWRITEBYTECODE=1 python3 "$(command -v behave)" --tags=-skip -D dump_errors=true @feature_list.txt $@
 
