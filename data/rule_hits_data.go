21a8a941d624ed1c4f04907e4391bc60d34dc03e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-27 12:39:45 +0200

    Rule hits data

---
+++ data/rule_hits_data.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2021 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package data
+
+import (
+	"github.com/RedHatInsights/insights-results-aggregator-mock/types"
+)
+
+var ruleHits []types.RuleHit = []types.RuleHit{
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-624a-49a5-bab8-4fdc5e51a266"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-6577-4e80-85e7-697cb646ff37"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-8933-4a3a-8634-3328fe806e08"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-8d6a-43cc-b82c-7007664bdf69"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-0000-0000-0000-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-1111-1111-1111-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-2222-2222-2222-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-3333-3333-3333-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-4444-4444-4444-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-5555-5555-5555-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-6666-6666-6666-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-7777-7777-7777-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-8888-8888-8888-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-9999-9999-9999-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-aaaa-aaaa-aaaa-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-bbbb-bbbb-bbbb-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-cccc-cccc-cccc-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-dddd-dddd-dddd-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-ffff-ffff-ffff-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "00000001-ffff-ffff-ffff-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "34c3ecc5-624a-49a5-bab8-4fdc5e51a266"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "74ae54aa-6577-4e80-85e7-697cb646ff37"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "a7467445-8d6a-43cc-b82c-7007664bdf69"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_requirements_check.report", "NODES_MINIMUM_REQUIREMENTS_NOT_MET", "ee7d2bf4-8933-4a3a-8634-3328fe806e08"},
+
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-6577-4e80-85e7-697cb646ff37"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-8d6a-43cc-b82c-7007664bdf69"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-1111-1111-1111-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-3333-3333-3333-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-5555-5555-5555-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-7777-7777-7777-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-9999-9999-9999-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-bbbb-bbbb-bbbb-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-dddd-dddd-dddd-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "00000001-ffff-ffff-ffff-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "74ae54aa-6577-4e80-85e7-697cb646ff37"},
+	types.RuleHit{"ccx_rules_ocp.external.bug_rules.bug_1766907.report", "BUGZILLA_BUG_1766907", "ee7d2bf4-8933-4a3a-8634-3328fe806e08"},
+
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_kubelet_version_check.report", "NODE_KUBELET_VERSION", "00000001-8d6a-43cc-b82c-7007664bdf69"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_kubelet_version_check.report", "NODE_KUBELET_VERSION", "00000001-3333-3333-3333-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_kubelet_version_check.report", "NODE_KUBELET_VERSION", "00000001-7777-7777-7777-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_kubelet_version_check.report", "NODE_KUBELET_VERSION", "00000001-bbbb-bbbb-bbbb-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_kubelet_version_check.report", "NODE_KUBELET_VERSION", "00000001-ffff-ffff-ffff-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.nodes_kubelet_version_check.report", "NODE_KUBELET_VERSION", "ee7d2bf4-8933-4a3a-8634-3328fe806e08"},
+
+	types.RuleHit{"ccx_rules_ocp.external.rules.samples_op_failed_image_import_check.report", "SAMPLES_FAILED_IMAGE_IMPORT_ERR", "00000001-8d6a-43cc-b82c-7007664bdf69"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.samples_op_failed_image_import_check.report", "SAMPLES_FAILED_IMAGE_IMPORT_ERR", "00000001-3333-3333-3333-000000000000"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.samples_op_failed_image_import_check.report", "SAMPLES_FAILED_IMAGE_IMPORT_ERR", "00000001-7777-7777-7777-000000000000"},
+
+	types.RuleHit{"ccx_rules_ocp.external.rules.cluster_wide_proxy_auth_check.report", "AUTH_OPERATOR_PROXY_ERROR", "00000001-8d6a-43cc-b82c-7007664bdf69"},
+	types.RuleHit{"ccx_rules_ocp.external.rules.cluster_wide_proxy_auth_check.report", "AUTH_OPERATOR_PROXY_ERROR", "00000001-3333-3333-3333-000000000000"},
+}
+
+func main() {
+}
