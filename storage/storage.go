cb1af3becd35f40426223df862b067d6fe15c424
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-11-25 16:40:28 +0100

    Fixed issue #281

--- storage/storage.go
+++ storage/storage.go
@@ -353,9 +353,7 @@ func (storage MemoryStorage) ReadReportForOrganizationAndCluster(
 	switch orgID {
 	case 11940171:
 		return types.ClusterReport(report), errors.New("You have no permissions to get or change info about this organization")
-	case 1:
-		fallthrough
-	case 2:
+	case 1, 2:
 		fallthrough
 	case 3:
 		fallthrough
