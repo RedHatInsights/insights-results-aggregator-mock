27cbce365a72a5e517d455491d979b2c46f1d590
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-16 10:15:38 +0200

    Endpoint implementation

--- server/dvo_handlers.go
+++ server/dvo_handlers.go
@@ -15,3 +15,57 @@ limitations under the License.
 */
 
 package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/rs/zerolog/log"
+)
+
+// allDVONamespaces handler returns list of all DVO namespaces. Currently it
+// does not depend on Organization ID as this information is passed through
+// Bearer token in real Smart Proxy service. The format of output should be:
+//
+//           {
+//             "status": "ok",
+//             "workloads": [
+//                 {
+//                     "cluster": {
+//                         "uuid": "{cluster UUID}",
+//                         "display_name": "{cluster UUID or displayable name}",
+//                     },
+//                     "namespace": {
+//                         "uuid": "{namespace UUID}",
+//                         "name": "{namespace real name}", // optional, might be null
+//                     },
+//                     "reports": [
+//                         {
+//                             "check": "{for example no_anti_affinity}", // taken from the original full name deploment_validation_operator_no_anti_affinity
+//                             "kind": "{kind attribute}",
+//                             "description": {description}",
+//                             "remediation": {remediation}",
+//                         },
+//                     ]
+//             ]
+//         }
+func (server *HTTPServer) allDVONamespaces(writer http.ResponseWriter, request *http.Request) {
+	log.Info().Msg("All DVO namespaces handler")
+
+	// prepare response structure
+	var responseData AllDVONamespacesResponse
+	responseData.Status = "ok"
+
+	// transform response structure into proper JSON payload
+	bytes, err := json.MarshalIndent(responseData, "", "\t")
+	if err != nil {
+		log.Error().Err(err).Msg(responseDataError)
+		return
+	}
+
+	// and send the response to client
+	_, err = writer.Write(bytes)
+	if err != nil {
+		log.Error().Err(err).Msg(responseDataError)
+	}
+}
