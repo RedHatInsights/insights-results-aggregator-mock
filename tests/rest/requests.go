32853e814e355f7d135a721cce70eb0ef8db4b34
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-29 15:29:06 +0200

    Structures representing request and response payloads

---
+++ tests/rest/requests.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2023 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tests
+
+import (
+	"time"
+)
+
+// RequestStruct represents one entry in list of requests
+type RequestStruct struct {
+	RequestID string    `json:"requestID"`
+	Valid     bool      `json:"valid"`
+	Received  time.Time `json:"received"`
+	Processed time.Time `json:processed"`
+}
+
+// RequestsResponse represents response containing list of requests
+type RequestResponse struct {
+	Cluster  string          `json:"cluster"`
+	Requests []RequestStruct `json:"requests"`
+	Status   string          `json:"status"`
+}
