243df64c3e262439e0e0a609cd6dfdd723b40a97
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-30 10:34:39 +0200

    Helper functions - predicates/checkers etc.

--- tests/rest/multiple_reports.go
+++ tests/rest/multiple_reports.go
@@ -16,6 +16,13 @@ limitations under the License.
 
 package tests
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/verdverm/frisby"
+)
+
 // MultipleReportsResponse represents response from the server that contains
 // results for multiple clusters together with overall status
 type MultipleReportsResponse struct {
@@ -29,4 +36,116 @@ type MultipleReportsResponse struct {
 // ClusterListInRequest represents request body containing list of clusters
 type ClusterListInRequest struct {
 	Clusters []string `json:"clusters"`
+}
+
+// constructURLForReportForOrgClustersPostMethod function construct an URL to
+// access the endpoint to retrieve results for given list of clusters using
+// POST method
+func constructURLForReportForOrgClustersPostMethod() string {
+	return fmt.Sprintf("%sclusters", apiURL)
+}
+
+// sendClusterListInPayload function sends the cluster list in request payload
+// to server
+func sendClusterListInPayload(f *frisby.Frisby, clusterList []string) {
+	var payload = ClusterListInRequest{
+		Clusters: clusterList,
+	}
+	// create payload
+	f.SetJson(payload)
+
+	// and perform send
+	f.Send()
+}
+
+// readMultipleReportsResponse reads and parses response body that should
+// contains reports for multiple clusters
+func readMultipleReportsResponse(f *frisby.Frisby) MultipleReportsResponse {
+	response := MultipleReportsResponse{}
+
+	// try to read response body
+	text, err := f.Resp.Content()
+
+	if err != nil {
+		f.AddError(err.Error())
+	} else {
+		// try to deserialize response body
+		err := json.Unmarshal(text, &response)
+		if err != nil {
+			f.AddError(err.Error())
+		}
+	}
+	return response
+}
+
+// expectNumberOfClusters utility function checks if server response contains
+// expected number of clusters
+func expectNumberOfClusters(f *frisby.Frisby, response MultipleReportsResponse, expected int) {
+	clusters := response.Clusters
+	actual := len(clusters)
+	if actual != expected {
+		f.AddError(fmt.Sprintf("expected %d clusters in server response, but got %d instead", expected, actual))
+	}
+}
+
+// expectNumberOfClusters utility function checks if server response contains
+// expected number of errors
+func expectNumberOfErrors(f *frisby.Frisby, response MultipleReportsResponse, expected int) {
+	clusters := response.Errors
+	actual := len(clusters)
+	if actual != expected {
+		f.AddError(fmt.Sprintf("expected %d errors in server response, but got %d instead", expected, actual))
+	}
+}
+
+// expectNumberOfReports utility function checks if server response contains
+// expected number of errors
+func expectNumberOfReports(f *frisby.Frisby, response MultipleReportsResponse, expected int) {
+	clusters := response.Reports
+	actual := len(clusters)
+	if actual != expected {
+		f.AddError(fmt.Sprintf("expected %d reports in server response, but got %d instead", expected, actual))
+	}
+}
+
+// expectClusterInResponse utility function checks if server response contains
+// expected cluster name
+func expectClusterInResponse(f *frisby.Frisby, response MultipleReportsResponse, clusterName string) {
+	clusters := response.Clusters
+	for _, cluster := range clusters {
+		// cluster has been found
+		if cluster == clusterName {
+			return
+		}
+	}
+	// cluster was not found
+	f.AddError(fmt.Sprintf("Cluster %s can not be found in server response in the cluster list", clusterName))
+}
+
+// expectClusterInResponse utility function checks if server response contains
+// expected report for specified cluster
+func expectReportInResponse(f *frisby.Frisby, response MultipleReportsResponse, clusterName string) {
+	reports := response.Reports
+	for cluster := range reports {
+		// cluster has been found
+		if cluster == clusterName {
+			return
+		}
+	}
+	// report for cluster was not found
+	f.AddError(fmt.Sprintf("Cluster %s can not be found in server response in reports map", clusterName))
+}
+
+// expectErrorClusterInResponse utility function checks if server response
+// contains expected error
+func expectErrorClusterInResponse(f *frisby.Frisby, response MultipleReportsResponse, clusterName string) {
+	errors := response.Errors
+	for _, cluster := range errors {
+		// cluster has been found
+		if cluster == clusterName {
+			return
+		}
+	}
+	// error for cluster was not found
+	f.AddError(fmt.Sprintf("Cluster %s can not be found in server response in the errors list", clusterName))
 }
