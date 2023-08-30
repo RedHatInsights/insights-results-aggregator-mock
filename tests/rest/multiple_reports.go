5d4e70fe01e40e4e7691f286583cc3d0fd68e9bd
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-30 10:36:42 +0200

    Check for multiple results: positive test case

--- tests/rest/multiple_reports.go
+++ tests/rest/multiple_reports.go
@@ -209,3 +209,34 @@ func checkMultipleReportsForKnownOrganizationAnd2KnownClustersUsingPostMethod() 
 
 	f.PrintReport()
 }
+
+// checkMultipleReportsForKnownOrganizationAnd3KnownClustersUsingPostMethod check the endpoint that returns multiple results
+func checkMultipleReportsForKnownOrganizationAnd3KnownClustersUsingPostMethod() {
+	clusterList := []string{
+		knownClusterForOrganization1,
+		knownCluster2ForOrganization1,
+		knownCluster3ForOrganization1,
+	}
+
+	// send request to the endpoint
+	url := constructURLForReportForOrgClustersPostMethod()
+	f := frisby.Create("Check the endpoint to return report for existing organization and three cluster IDs (POST variant)").Post(url)
+	sendClusterListInPayload(f, clusterList)
+
+	// check the response from server
+	f.ExpectHeader(contentTypeHeader, ContentTypeJSON)
+
+	// check the payload returned from server
+	response := readMultipleReportsResponse(f)
+	expectNumberOfClusters(f, response, 3)
+	expectNumberOfErrors(f, response, 0)
+	expectNumberOfReports(f, response, 3)
+	expectClusterInResponse(f, response, knownClusterForOrganization1)
+	expectClusterInResponse(f, response, knownCluster2ForOrganization1)
+	expectClusterInResponse(f, response, knownCluster3ForOrganization1)
+	expectReportInResponse(f, response, knownClusterForOrganization1)
+	expectReportInResponse(f, response, knownCluster2ForOrganization1)
+	expectReportInResponse(f, response, knownCluster3ForOrganization1)
+
+	f.PrintReport()
+}
