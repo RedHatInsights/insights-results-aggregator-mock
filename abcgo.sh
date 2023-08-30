fd754ee8777e8cf9936f3419b438a4e2a9fc98a4
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-30 13:46:51 +0200

    Increase threshold again

--- abcgo.sh
+++ abcgo.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-threshold=65
+threshold=75
 
 BLUE=$(tput setaf 4)
 RED_BG=$(tput setab 1)
