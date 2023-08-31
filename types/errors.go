af19ac86f8647ff1adb40c263da08951879be920
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-31 10:37:29 +0200

    Removed ItemNotFoundError

--- types/errors.go
+++ types/errors.go
@@ -31,16 +31,6 @@ import (
 // exists on the storage while attempting to write a report for a cluster.
 var ErrOldReport = errors.New("More recent report already exists in storage")
 
-// ItemNotFoundError shows that item with id ItemID wasn't found in the storage
-type ItemNotFoundError struct {
-	ItemID interface{}
-}
-
-// Error returns error string
-func (e *ItemNotFoundError) Error() string {
-	return fmt.Sprintf("Item with ID %+v was not found in the storage", e.ItemID)
-}
-
 // TableNotFoundError table not found error
 type TableNotFoundError struct {
 	tableName string
