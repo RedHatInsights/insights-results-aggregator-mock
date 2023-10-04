a58e046bcef8011694f7c96937b359f14fcd1aa7
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-04 13:46:08 +0200

    Fixed linter issue

--- server/dvo_handlers.go
+++ server/dvo_handlers.go
@@ -296,7 +296,7 @@ func getNamespaces(workloads []types.DVOWorkload) []string {
 
 	// convert map to slice of keys.
 	keys := []string{}
-	for key, _ := range namespaces {
+	for key := range namespaces {
 		keys = append(keys, key)
 	}
 
